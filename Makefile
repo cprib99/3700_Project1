67a020c3d56cedf12aacfadcd86cc77b75274ee0
Cameron <cprib99@gmail.com>
2020-02-11 13:28:41 -0500

    added prototypes and definitions fo findOutsidePoint and intersect

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
-main: fraction.o main.o point.o
-	g++ -o main fraction.cc main.cc point.cc
+project1: main.o point.o fraction.o
+	g++ -o project1 main.cc point.cc fraction.cc
+
+main.o: main.cc point.h fraction.h
+	g++ -c main.cc
+
+point.o: point.cc point.h fraction.h
+	g++ -c point.cc
 
 fraction.o: fraction.cc fraction.h
 	g++ -c fraction.cc
 
-main.o: main.cc fraction.h point.h
-	g++ -c main.cc
-
-point.o: point.cc point.h
-	g++ -c point.cc
--- main.cc
+++ main.cc
@@ -6,6 +6,80 @@
 #include "point.h"
 using namespace std;
 
+//global variable numPoints - used in fcn definitions to know how many points to iterate for
+int numPoints;
+//global var fraction1 for converting values to fractions
+Fraction fraction1 = (1,1);
+Fraction fraction0 = (0,0);
+
+//Algorithm 1 - find point outside of polygon Q
+Point findOustidePoint(Point Q[], Point p1)
+{
+  //get yMax point
+  Fraction yTemp;
+
+  for(int i=0; i<numPoints; i++)
+  {
+    if(Q[i].getY().operator>=(yTemp))
+    {
+      yTemp = Q[i].getY();
+    }
+  }
+
+  Fraction x;
+  yTemp.operator=(yTemp.operator+(fraction1));
+
+  cout << yTemp << endl;
+
+  Point p2(x,yTemp);
+  bool good = false;
+
+  //loop for finding yMax
+  while(good = false)
+  {
+    x = x + fraction1;
+    good = true;
+    for (int i=0; i<(numPoints-1); i++)
+    {
+      if(((Q[i].operator-(p1)).operator*(p2.operator-(p1)) == 0))
+      {
+        good = false;
+      }
+    }
+  }
+  return(x,yTemp);
+}
+
+bool intersect(Point p1, Point p2, Point q1, Point q2)
+{
+  Point r, s, v;
+  Fraction d, t, u, vs, vr;
+  r = p2.operator-(p1);
+  s = q2.operator-(q1);
+  v = q1.operator-(p1);
+  d = r.operator*(s);
+
+  if(d.operator==(fraction0))
+  {
+    return false;
+  }
+  else
+  {
+    vs.operator=(v.operator*(s));
+    vr.operator=(v.operator*(r));
+    t.operator=(vs.operator/(d));
+    u.operator=(vr.operator/(d));
+    if(((t.operator>=(fraction0) && t.operator<=(fraction1)) && ((u.operator>=(fraction0) && (u.operator>=(fraction1))))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
+
 int main()
 {
     //open file in program
@@ -24,7 +98,13 @@ int main()
     }
     testPoint = pointData[numPoints];
 
-    cout << testPoint << "Test" << endl;;
+//test section below
+    //cout << testPoint << endl;;
+
+    Point outsidePoint;
+    outsidePoint = findOustidePoint(pointData, testPoint);
+    cout << outsidePoint << endl;
+
 
 
     return 0;
